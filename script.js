17ed7e269d9efbf60f72db465e3bc86e3d45dd73
Harris Nguyen <hainx1610@gmail.com>
2022-11-13 11:10:21 +0700

    create operate() function to add, subtract, multiply, divide

--- script.js
+++ script.js
@@ -4,4 +4,23 @@ const equalsButton = document.querySelector("[data-equals]");
 const deleteButton = document.querySelector("[data-delete]");
 const allClearButton = document.querySelector("[data-all-clear]");
 const previousOperandElement = document.querySelector("[data-previous-operand]");
-const cureentOperandElement = document.querySelector("[data-current-operand]");
+const currentOperandElement = document.querySelector("[data-current-operand]");
+
+const calcAdd = (a, b) => +a + +b;
+const calcSubstract = (a, b) => +a - +b;
+const calcMultiply = (a, b) => +a * +b;
+const calcDivide = (a, b) => +a / +b;
+
+function operate(operator, a, b) {
+    let result;
+    if (operator === "+") {
+        result = calcAdd(a, b);
+    } else if (operator === "-") {
+        result = calcSubstract(a, b);
+    } else if (operator === "*"){
+        result = calcMultiply(a, b);
+    } else if (operator === "/") {
+        result = calcDivide(a, b);
+    };
+    return result;
+};
