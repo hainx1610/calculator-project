22531736b82421ac7e27c5971702c3d1304f847d
Harris Nguyen <hainx1610@gmail.com>
2022-11-15 10:37:09 +0700

    change operate() function to remove the need for parameters and store the values of current, previous, and operation

--- script.js
+++ script.js
@@ -1,3 +1,55 @@
+const calcAdd = (a, b) => +a + +b;
+const calcSubstract = (a, b) => +a - +b;
+const calcMultiply = (a, b) => +a * +b;
+const calcDivide = (a, b) => +a / +b;
+
+function operate() {
+    let result;
+    const a = parseFloat(previousOperand);
+    const b = parseFloat(currentOperand);
+    if (isNaN(a) || isNaN(b)) return; // stop function if a or b is an empty string
+    if (operation === "+") {
+        result = calcAdd(a, b);
+    } else if (operation === "-") {
+        result = calcSubstract(a, b);
+    } else if (operation === "*"){
+        result = calcMultiply(a, b);
+    } else if (operation === "/") {
+        result = calcDivide(a, b);
+    };
+    clear();
+    currentOperand = result;
+};
+
+function clear() {
+    currentOperand = "";
+    previousOperand = "";
+    operation = undefined;
+}
+
+function deleteNumber() {
+
+}
+
+function appendNumber(number) {
+    currentOperand += number;
+}
+
+function chooseOperation(op) {
+    if (currentOperand === "") return // not execute function if current is empty
+    if (previousOperand !== "") {
+        operate();
+    }
+    operation = op;
+    previousOperand = currentOperand;
+    currentOperand = "";
+}
+
+function updateDisplay() {
+    currentOperandElement.textContent = currentOperand;
+    previousOperandElement.textContent = previousOperand;
+}
+
 const numberButtons = document.querySelectorAll("[data-number]");
 const operationButtons = document.querySelectorAll("[data-operation]");
 const equalsButton = document.querySelector("[data-equals]");
@@ -6,21 +58,37 @@ const allClearButton = document.querySelector("[data-all-clear]");
 const previousOperandElement = document.querySelector("[data-previous-operand]");
 const currentOperandElement = document.querySelector("[data-current-operand]");
 
-const calcAdd = (a, b) => +a + +b;
-const calcSubstract = (a, b) => +a - +b;
-const calcMultiply = (a, b) => +a * +b;
-const calcDivide = (a, b) => +a / +b;
+let currentOperand;
+let previousOperand;
+let operation;
 
-function operate(operator, a, b) {
-    let result;
-    if (operator === "+") {
-        result = calcAdd(a, b);
-    } else if (operator === "-") {
-        result = calcSubstract(a, b);
-    } else if (operator === "*"){
-        result = calcMultiply(a, b);
-    } else if (operator === "/") {
-        result = calcDivide(a, b);
-    };
-    return result;
-};
+for (const button of numberButtons) {
+    button.addEventListener("click", () => {
+        appendNumber(button.textContent);
+        updateDisplay();
+    })
+}
+
+for (const button of operationButtons) {
+    button.addEventListener("click", () => {
+        chooseOperation(button.textContent);
+        updateDisplay();
+    })
+}
+
+equalsButton.addEventListener("click", () => {
+    operate();
+    updateDisplay();
+})
+
+allClearButton.addEventListener("click", () => {
+    clear();
+    updateDisplay();
+})
+
+deleteButton.addEventListener("click", () => {
+    deleteNumber();
+    updateDisplay();
+})
+
+clear();
